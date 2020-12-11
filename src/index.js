00cc6bbd35ad508719425df9383727dd800235fb
MeeraMeera1 <gatesameera98@gmail.com>
2020-12-11 18:13:44 -0500

    Basically learned how to use javascript variables in JSX

--- src/index.js
+++ src/index.js
@@ -8,9 +8,32 @@ a componenet is a function or a class
     produces HTML to show the user - using jsx
     handles feedback from the user - using eventHandlers
 */
+function getButtonText() {
+  return "Click On Me!";
+}
+
 const App = () => {
-  return <div>Hi There!</div>;
+
+  const buttonText = "Click Me!";
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 //take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+/*
+JSX vs HTML
+    adding custom styling to an element uses different syntax
+    adding a class to an element uses different syntax
+    JSX can reference JS variables
+*/
